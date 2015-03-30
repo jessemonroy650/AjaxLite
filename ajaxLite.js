af14b1659932ac7319cf7759a3b420c61cfa103d
Jesse Monroy <jessemonroy650@yahoo.com>
2015-03-30 00:49:38 -0700

    update ajaxlite.js

--- ajaxLite.js
+++ ajaxLite.js
@@ -1,30 +1,41 @@
 // Date: 2013-08-29 - directly from: http://www.w3schools.com/ajax/ajax_xmlhttprequest_create.asp
 // Rewrite: 2015-03-29 - Decide to make this for mobile only. Ignore IE 5 & 6.
+// 2015-03-30T00:36:08 - Decide to do the POST, PUT, & PATCH after all. Let's call this version 0.9.
+// A good reference on HTTP is CURL http://curl.haxx.se/docs/manpage.html
+// For JSON, see http://json.org/
 var AJAX = {
-	jsonhttp: null,
 
-	init : function (callback) {
-		// It's not clear if this will work:
+	xhttp   : null,
+	// list of pairs as need, all expected to be in the HTTP protocol
+	// SEE: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+	headers : [{'' : ''}],
+
+	init : function (callback, headers) {
+		// It's not clear this will work, so it's not implemented:
 		// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#XMLHttpRequest%28%29
 		// https://bugzilla.mozilla.org/show_bug.cgi?id=692677#c68
-		jsonhttp = new XMLHttpRequest();
+		xhttp = new XMLHttpRequest();
 		if (typeof callback == 'function') {
-			jsonhttp.onreadystatechange = function() {
-				if (jsonhttp.readyState === 4 && jsonhttp.status === 200) {
-					callback(jsonhttp.responseText);
+			xhttp.onreadystatechange = function() {
+				if (xhttp.readyState === 4 && xhttp.status === 200) {
+					callback(xhttp.responseText);
+				} else {
+					// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#getAllResponseHeaders%28%29
+					//alert(xhttp.getAllResponseHeaders());
 				}
 			}
 		} else if (typeof callback == 'string') {
-			jsonhttp.onreadystatechange = function() {
-				if (jsonhttp.readyState === 4 && jsonhttp.status === 200) {
-					document.getElementById(callback).innerHTML = jsonhttp.responseText;
+			xhttp.onreadystatechange = function() {
+				if (xhttp.readyState === 4 && xhttp.status === 200) {
+					document.getElementById(callback).innerHTML = xhttp.responseText;
+				} else {
+					//alert(xhttp.getAllResponseHeaders());
 				}
 			}
 		} else {
 			alert('AJAX.init, No tag or callback');
 		}
 	},
-
 	// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
 	// On ''async''
 	// An optional boolean parameter, defaulting to true, indicating whether or
@@ -35,15 +46,44 @@ var AJAX = {
 	// attribute is true, or an exception will be thrown. 
 	GET : function (URL) {
 		async = true; // This is explicit for clarity.
-		jsonhttp.open('GET', URL, async);
-		jsonhttp.send();
+		xhttp.open('GET', URL, async);
+		xhttp.send();
 		// After this the callback handles the returning data.
+	},
+	//
+	POST : function (URL, data) {
+		async = true; // This is explicit for clarity.
+		xhttp.open("POST", URL, true);
+		if ( this.headers.length > 0 ) {
+			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		}
+		xhttp.send(data);
+	},
+	//
+	PUT : function (URL, data) {
+		async = true; // This is explicit for clarity.
+		xhttp.open("PUT", URL, true);
+		if ( this.headers.length > 0 ) {
+			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		}
+		xhttp.send(data);
+	},
+	//
+	PATCH : function (URL, data) {
+		async = true; // This is explicit for clarity.
+		xhttp.open("PATCH", URL, true);
+		if ( this.headers.length > 0 ) {
+			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		}
+		xhttp.send(data);
+	},
+	//
+	DELETE : function (URL) {
+		async = true; // This is explicit for clarity.
+		xhttp.open("DELETE", URL, true);
+		if ( this.headers.length > 0 ) {
+			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		}
+		xhttp.send();
 	}
-	// HOW TO DO a POST method:
-	// POST : function (URL, data) {
-	//	async = true; // This is explicit for clarity.
-	//	jsonhttp.open("POST", URL, true);
-	//	jsonhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	//	jsonhttp.send(data);
-	// }
 }
